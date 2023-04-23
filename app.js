5a14bf2a5538f560570930d576534bae7d7c2ca3
Egbeleke Daniel <fiyinfoluwaegbeleke@gmail.com>
2023-04-23 14:12:38 +0100

    ai api integration

--- app.js
+++ app.js
@@ -8,10 +8,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
 
 app.get("/", (req, res) => {
   res.send("Server Running");
 });
+
+
 
 
 const port = process.env.ACCESS_PORT || 6500;
