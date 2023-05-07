a54d73590ba14d23994ec029c130078766c949ba
Egbeleke Daniel <fiyinfoluwaegbeleke@gmail.com>
2023-05-07 11:20:56 -0700

    backend fixes

--- laughable-delimma-app/app.js
+++ laughable-delimma-app/app.js
@@ -12,22 +12,34 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const configuration = new Configuration({
-  apiKey: process.env.OPEN_AI_KEY,
+  apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
 
 app.post("/chat", async (req, res) => {
     const{prompt} = req.body;
+    try{
+        const completion = await openai.createCompletion({
+          model: "text-davinci-003",
+          max_tokens: 512,
+          temperature: 1,
+          prompt:
+            `You are to unapologetically mean and decisive and ask upfront questions if need be when replying what i say here: ${prompt}`,
+        });
 
-    const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        max_tokens: 512,
-        temperature: 1,
-        prompt: prompt,
-    });
+    // You are to unapologetically mean and decisive and ask upfront questions if need be when replying what i say here: ${prompt}
 
     res.send(completion.data.choices[0].text);
+    console.log(completion.data.choices[0].text);
+
+
+    } catch(e){
+        console.log(e);
+    }
+    
+    // console.log(completion.data.choices[0].text);
+
 });
 
 const port = process.env.ACCESS_PORT || 6500;
